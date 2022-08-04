bab504ead1838f08cdee83139340787ab88b8228
Татьяна Смыслова <l.dalecrua@gmail.com>
2022-08-04 23:11:40 +0300

    test

--- src/test/java/ru/netology/test/test.java
+++ src/test/java/ru/netology/test/test.java
@@ -16,7 +16,6 @@ public class test {
 
     @BeforeAll
     static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
         System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
